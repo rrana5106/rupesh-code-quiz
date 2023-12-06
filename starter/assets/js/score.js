221c287a42bade18279114de2d5ae0129ea70b8d
rrana5106 <rrana5106@hotmail.com>
2023-12-06 00:33:53 +0000

    updated

--- starter/assets/js/score.js
+++
@@ -1,6 +0,0 @@
-// Function to end the quiz and display the final score
-function endQuiz() {
-    questionId.classList.add("hide");
-    feedbackForAns.classList.remove("hide");
-    feedbackForAns.textContent = "Quiz ended! Your final score is: " + timeLeft + " seconds";
-  }
